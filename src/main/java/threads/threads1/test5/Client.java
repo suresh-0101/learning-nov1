35d7aecd4e4d9a77ace0fa2a59c81353708c69f2
suresh <sureshm.star369@gmail.com>
2025-11-16 23:35:55 +0530

    readme problem statement added

--- src/main/java/threads/threads1/test5/Client.java
+++ src/main/java/threads/threads1/test5/Client.java
@@ -1,13 +1,5 @@
 package threads.threads1.test5;
 
-//**Raw Problem**
-//Write code to achieve the following
-//A class Client with main method that prints: I am the main class
-//Client class should create a new thread and invoke code in a class called Adder.
-//The Adder class should print: I am the Adder class
-//Client class should create a new thread and invoke code in a class called Subtractor.
-//The Subtractor class should print: I am the Subtractor class
-
 public class Client {
     public static void main(String[] args) {
 
---
+++ src/main/java/threads/threads1/test5/readme.txt
@@ -0,0 +1,13 @@
+**Raw Problem**
+Write code to achieve the following
+
+A class Client with main method that prints:
+        I am the main class
+
+Client class should create a new thread and invoke code in a class called Adder.
+The Adder class should print:
+        I am the Adder class
+
+Client class should create a new thread and invoke code in a class called Subtractor.
+The Subtractor class should print:
+        I am the Subtractor class
